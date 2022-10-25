26d2f0e2fcad9a7355a23606499c71e9a4133676
Ludwig Nojd <ludwig.nojd@hotmail.se>
2022-10-25 13:48:14 +0200

    Dropdown functionality working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css"
+import arrowDown from "./img/arrow-down.svg"
+import arrowUp from "./img/arrow-up.svg"
 import { useState, useEffect } from "react"
 
 function App() {
   const [drop, setDrop] = useState(false)
   const [cities, setCities] = useState(null)
+  const [showCity, setShowCity] = useState(null)
 
   useEffect(() => {
     fetch("https://avancera.app/cities/")
@@ -14,19 +17,47 @@ function App() {
       })
   }, [])
 
+  function filterById(id) {
+    if (cities) {
+      const tempName = cities.filter((current) => current.id === id)
+      setShowCity(tempName)
+      console.log(showCity)
+    }
+  }
+
   return (
     <div className="App">
       <div className="wrapper">
         <div onClick={() => setDrop(true)} className="drop-down">
-          <h4>Pick something</h4>
+          {showCity !== null ? (
+            showCity.map((city) => <h4 key={city.id}>{city.name}</h4>)
+          ) : (
+            <h4>Pick a city</h4>
+          )}
+
+          {/* {showCity?.map((city) => (
+            <h4 key={city.id}>{city.name}</h4>
+
+          ): (
+            <h4>Pick something</h4>
+          )} */}
+
+          {drop !== false ? (
+            <img src={arrowUp} alt="" />
+          ) : (
+            <img src={arrowDown} alt="" />
+          )}
         </div>
         {drop !== false ? (
           <div onMouseLeave={() => setDrop(false)} className="drop-down-menu">
-            {cities?.map((city) => (
-              <ul>
-                <li key={city.id}>{city.name}</li>
-              </ul>
-            ))}
+            <ul>
+              {" "}
+              {cities?.map((city) => (
+                <li onClick={() => filterById(city.id)} key={city.id}>
+                  <h4 className="list-item">{city.name}</h4>
+                </li>
+              ))}
+            </ul>
           </div>
         ) : (
           ""
