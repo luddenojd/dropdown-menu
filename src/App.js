0b429d9520bce3705647d401dd54e55e8ff8a505
Ludwig Nojd <ludwig.nojd@hotmail.se>
2022-10-25 12:07:32 +0200

    Fetch data, style, states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useState, useEffect } from "react"
 
 function App() {
+  const [drop, setDrop] = useState(false)
+  const [cities, setCities] = useState(null)
+
+  useEffect(() => {
+    fetch("https://avancera.app/cities/")
+      .then((response) => response.json())
+      .then((result) => {
+        setCities(result)
+        console.log(result)
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <div onClick={() => setDrop(true)} className="drop-down">
+          <h4>Pick something</h4>
+        </div>
+        {drop !== false ? (
+          <div onMouseLeave={() => setDrop(false)} className="drop-down-menu">
+            {cities?.map((city) => (
+              <ul>
+                <li key={city.id}>{city.name}</li>
+              </ul>
+            ))}
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
